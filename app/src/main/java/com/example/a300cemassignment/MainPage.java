53ce81950b0b7017bf8600956c8083a1e32ebe94
Zakariyya Ahmed <ahmedz27@coventry.ac.uk>
2019-12-12 09:55:23 +0000

    MainPage Code Commented

--- app/src/main/java/com/example/a300cemassignment/MainPage.java
+++ app/src/main/java/com/example/a300cemassignment/MainPage.java
@@ -1,15 +1,14 @@
 package com.example.a300cemassignment;
 
+//Import Statements for all imported interfaces in this class
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-
 import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
 
 public class MainPage extends AppCompatActivity {
-
+    //Private Variables for the Buttons
     private Button BuildingView;
     private Button WeatherView;
 
@@ -18,6 +17,7 @@ public class MainPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
 
+        //Button to load the University Buildings activity
         BuildingView = (Button) findViewById(R.id.navBuilding);
         BuildingView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,6 +26,7 @@ public class MainPage extends AppCompatActivity {
             }
         });
 
+        //Button to load the Activity with the Weather API
         WeatherView = (Button) findViewById(R.id.navNotes);
         WeatherView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,14 +36,13 @@ public class MainPage extends AppCompatActivity {
         });
 
     }
-
+    //Intent to start the Activity
     public void openHomePage() {
         Intent intent = new Intent(this, HomePage.class);
         startActivity(intent);
 
     }
-
-
+    //Intent to start the Activity
     public void openDisplayWeather(){
         Intent intent = new Intent(this, DisplayWeather.class);
         startActivity(intent);
