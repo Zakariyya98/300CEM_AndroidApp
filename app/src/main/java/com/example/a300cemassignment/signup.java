5ffd1ee97b513c27488a3fcabf3bb2cc46715ec5
Zakariyya Ahmed <ahmedz27@coventry.ac.uk>
2019-12-12 09:58:33 +0000

    signup Code Commented

--- app/src/main/java/com/example/a300cemassignment/signup.java
+++ app/src/main/java/com/example/a300cemassignment/signup.java
@@ -1,19 +1,15 @@
 package com.example.a300cemassignment;
 
+//Import Statements for all imported interfaces in this class
 import android.os.Bundle;
 import android.content.Intent;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
 import android.view.View;
 import android.widget.EditText;
 
 public class signup extends AppCompatActivity {
 
+    //Private Statements used for User Signup when they signup on the app
     private EditText userID;
     private EditText usernameSignup;
     private EditText passwordSignup;
@@ -23,11 +19,13 @@ public class signup extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
+        //These Variables are where the data gets entered and pulled from
         userID = (EditText) findViewById(R.id.userID);
         usernameSignup = (EditText) findViewById(R.id.usernameSignup);
         passwordSignup = (EditText) findViewById(R.id.passwordSignup);
     }
-
+    //Method to convert the input to a string which will then be passed to the
+    //database ready to be stored.
     public void save(View v){
         int anuserID = Integer.parseInt(userID.getText().toString());
         String aUsername = usernameSignup.getText().toString();
